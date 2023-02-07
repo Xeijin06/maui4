cd4668ddcb0fbb97dd230c395d5b16f018d8bc0a
Luis González <lgxei06@gmail.com>
2023-02-07 16:06:55 -0500

    Init

---
+++ MauiApp4/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using MauiApp4.Helper;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace MauiApp4;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureMauiHandlers(handlers =>
+            {
+#if __ANDROID__
+                handlers.AddHandler(typeof(CustomFrame), typeof(MauiApp4.Droid.Renderers.CustomFrameShadowRenderer));
+#endif
+
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
---
+++ MauiApp4/Platforms/Android/Helper/CustomFrameShadowRenderer.cs
@@ -0,0 +1,43 @@
+﻿using Android.Content;
+using MauiApp4.Droid.Renderers;
+using MauiApp4.Helper;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ExportRenderer(typeof(CustomFrame), typeof(CustomFrameShadowRenderer))]
+namespace MauiApp4.Droid.Renderers
+{
+    public class CustomFrameShadowRenderer : Microsoft.Maui.Controls.Handlers.Compatibility.FrameRenderer
+    {
+        public CustomFrameShadowRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+            var element = e.NewElement as CustomFrame;
+
+
+            if (element == null) return;
+
+            if (element.HasShadow)
+            {
+                Android.Graphics.Color color = new Android.Graphics.Color(0, 0, 0, 255);
+                
+                SetOutlineSpotShadowColor(color);
+                SetOutlineAmbientShadowColor(color);
+                Elevation = 30.0f;
+                TranslationZ = 0.0f;
+                SetZ(30f);
+            }
+
+        }
+
+    }
+}
